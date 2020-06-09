418f00bcc44c04ce122ad350939719a773dc7506
Nikolay Burlov <kohgpat@gmail.com>
2020-06-10 00:59:09 +0800

    add todo actions dropdown

--- src/modules/Todos/components/Todo/index.js
+++ src/modules/Todos/components/Todo/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import cn from "classnames";
+import { FiMoreHorizontal } from "react-icons/fi";
+import Downshift from "downshift";
 import Button from "../../../../components/Button";
 import s from "./index.module.css";
 import Checkbox from "../../../../components/Checkbox";
@@ -10,9 +13,63 @@ const Todo = ({ todo, onCheck, removeTodo }) => {
 
       <span className={s.text}>{todo.text}</span>
 
-      <Button className={s.removeButton} onClick={() => removeTodo(todo)}>
-        &times;
-      </Button>
+      <Downshift>
+        {({
+          getToggleButtonProps,
+          getMenuProps,
+          getItemProps,
+          isOpen,
+          highlightedIndex,
+        }) => {
+          return (
+            <div className={s.actions}>
+              <Button {...getToggleButtonProps()} className={s.actionsToggle}>
+                <FiMoreHorizontal />
+              </Button>
+
+              {isOpen ? (
+                <ul {...getMenuProps()} className={s.actionsList}>
+                  <li
+                    {...getItemProps({
+                      key: "edit",
+                      index: 0,
+                      item: "edit",
+                    })}
+                    className={cn(
+                      s.actionsListItem,
+                      highlightedIndex === 0 && s.actionsListItemHighlighted
+                    )}
+                  >
+                    <Button type="button" className={s.actionsListItemControl}>
+                      Edit
+                    </Button>
+                  </li>
+
+                  <li
+                    {...getItemProps({
+                      key: "delete",
+                      index: 1,
+                      item: "delete",
+                    })}
+                    className={cn(
+                      s.actionsListItem,
+                      highlightedIndex === 1 && s.actionsListItemHighlighted
+                    )}
+                  >
+                    <Button
+                      type="button"
+                      className={s.actionsListItemControl}
+                      onClick={() => removeTodo(todo)}
+                    >
+                      Delete
+                    </Button>
+                  </li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Downshift>
     </div>
   );
 };
