496cab5bc7f410d33cd7b81c2fccefef1e866775
Nikolay Burlov <kohgpat@gmail.com>
2020-06-17 16:29:53 +0800

    Disabled dark theme for later improvement

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { ThemeProvider } from "./contexts/Theme";
 import { TodosProvider } from "./contexts/Todos";
 import Todos from "./modules/Todos";
-import useDarkMode from "./hooks/useDarkMode";
+// import useDarkMode from "./hooks/useDarkMode";
 
 function App() {
-  const theme = useDarkMode() ? "dark" : "light";
+  // const theme = useDarkMode() ? "dark" : "light";
+  const theme = "light";
 
   return (
     <ThemeProvider theme={theme}>
